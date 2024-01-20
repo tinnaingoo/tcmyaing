e827650be69def2022a357190dc387d361cb5639
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-01-20 16:06:55 +0630

    Update open-slider.js

--- js/open-slider.js
+++ js/open-slider.js
@@ -1,3 +1,4 @@
+</script>
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -7,3 +8,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+</script>
