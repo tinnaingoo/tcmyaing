c27dd9f8ef19a979a7768dc3d82cc32aab5f7f36
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2025-03-26 19:38:38 +0630

    Update clock.js
    Signed-off-by: Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,23 +4,6 @@ const hourHand = document.querySelector(".hour"),
   secondHand = document.querySelector(".second");
   
 
-// check if the mode is already set to "Dark Mode" in localStorage
-//if (localStorage.getItem("mode") === "Dark Mode") {
-  // add "dark" class to body and set modeSwitch text to "Light Mode"
- // body.classList.add("dark");
-  //modeSwitch.textContent = "Light Mode";
-//}
-// add a click event listener to modeSwitch
-//modeSwitch.addEventListener("click", () => {
-  // toggle the "dark" class on the body element
-  //body.classList.toggle("dark");
-  // check if the "dark" class is currently present on the body element
-  //const isDarkMode = body.classList.contains("dark");
-  // set modeSwitch text based on "dark" class presence
-  //modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
-  // set localStorage "mode" item based on "dark" class presence
- // localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
-//});
 
 const updateTime = () => {
   // Get current time and calculate degrees for clock hands
