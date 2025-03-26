00b214af697152c40526c1d4d7b575fde0b6c9ce
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2025-03-26 09:07:07 +0630

    Update clock.js
    Signed-off-by: Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>

--- scripts/clock.js
+++ scripts/clock.js
@@ -3,7 +3,7 @@ const body = document.querySelector("body"),
   hourHand = document.querySelector(".hour"),
   minuteHand = document.querySelector(".minute"),
   secondHand = document.querySelector(".second"),
-  modeSwitch = document.querySelector(".mode-switch");
+  
 
 // check if the mode is already set to "Dark Mode" in localStorage
 //if (localStorage.getItem("mode") === "Dark Mode") {
